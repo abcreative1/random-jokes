c2f760bcea842ef3373c4c2795c77295db56fd3f
abdulxyz <uabdulrahman106@gmail.com>
2023-10-12 15:10:33 +0100

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+getJoke()
+
 function getJoke() {
 
     const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious&type=single"
@@ -19,4 +21,41 @@ function getJoke() {
             jokeElement.innerText = error
             console.log("Error: ", error);
         })
+}
+
+function copyJoke() {
+    const textToCopy = document.getElementById("jokes");
+    const copyBtn = document.getElementById("copy");
+
+    const additionalText = "More Jokes at https://abdulxyz.github.io/random-jokes/";
+
+    copyBtn.addEventListener("click", () => {
+
+        const originalText = textToCopy.textContent;
+
+
+        const textarea = document.createElement("textarea");
+        textarea.value = originalText + "   " + additionalText;
+
+
+
+        document.body.appendChild(textarea);
+
+        // Select the text in the textarea
+        textarea.select();
+
+        // Execute the copy command to copy the selected text to the clipboard
+        document.execCommand("copy");
+
+        // Remove the temporary textarea element
+        document.body.removeChild(textarea);
+
+        // Optionally, provide user feedback
+        copyBtn.textContent = "Copied!";
+        setTimeout(function () {
+            copyBtn.textContent = "Copy";
+        }, 2000);
+
+    })
+
 }
